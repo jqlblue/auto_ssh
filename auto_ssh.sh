df0065965f3b597dbf9f56b9ea86b6952aead81d
gaoyuan <gaoyuan@zhuqu.com>
2014-07-29 18:53:56 +0800

    add user profile config

--- auto_ssh.sh
+++ auto_ssh.sh
@@ -22,134 +22,157 @@
 #auto_ssh.sh -c category
 #auto_ssh.sh
 _current_dir=$(cd "$(dirname "$0")"; pwd)
+_port=22
 d_get_help()
 {
     usage="Usage $0 [-l host] [-u user] [-p pass]"
     echo ${usage}
     #todo restruct it later
 }
-while getopts ":hc:i:l:a" opt; do
-  case $opt in
-    c )
-      __category=$OPTARG;;
-    i )
-      __alias=$OPTARG;;
-    l )
-      __host=$OPTARG;;
-    p )
-      __pass=$OPTARG;;
-    u )
-      __user=$OPTARG;;
-    a )
-      __is_add=1;;
-    h )
-      d_get_help
-      exit 0;;
-    : ) 
-      echo ">>> Error: '-$OPTARG' requires an argument"
-      exit 1;;
-    ? ) 
-      echo ">>> Error: '-$OPTARG' not supported"
-      echo $usage
-      exit 1;;
-  esac
+while getopts ":hc:i:l:u:p:P:a" opt; do
+    case $opt in
+        c )
+            __category=$OPTARG;;
+        i )
+            __alias=$OPTARG;;
+        l )
+            __host=$OPTARG;;
+        P )
+            __port=$OPTARG;;
+        p )
+            __pass=$OPTARG;;
+        u )
+            __user=$OPTARG;;
+        a )
+            __is_add=1;;
+        h )
+            d_get_help
+            exit 0;;
+        : ) 
+            echo ">>> Error: '-$OPTARG' requires an argument"
+            exit 1;;
+        ? ) 
+            echo ">>> Error: '-$OPTARG' not supported"
+            echo $usage
+            exit 1;;
+    esac
 done
 shift $(($OPTIND - 1))
 
 d_get_category() {
-  declare -a categorys
-  local categorys="($($_current_dir/utility/config.py))"
-  local i=0
-  local category_len=${#categorys[@]}
-  if [[ $category_len == 0 ]];then
-    echo "get categorys falied"
-    exit 1
-  fi
-  while [[ $i -lt $category_len ]]
-  do
-    echo -e "\t$i) ${categorys[$i]}"
-    let i++
-  done
-  read category_no
-  if [[ ${categorys[$category_no]} == '' ]]
-  then
-    echo 'unknow category'
-    exit 1
-  fi
-  _category=${categorys[$category_no]}
+    declare -a categorys
+    local categorys="($($_current_dir/utility/config.py))"
+    local i=0
+    local category_len=${#categorys[@]}
+    if [[ $category_len == 0 ]];then
+        echo "get categorys falied"
+        exit 1
+    fi
+    while [[ $i -lt $category_len ]]
+    do
+        echo -e "\t$i) ${categorys[$i]}"
+        let i++
+    done
+    read category_no
+    if [[ ${categorys[$category_no]} == '' ]]
+    then
+        echo 'unknow category'
+        exit 1
+    fi
+    _category=${categorys[$category_no]}
 }
 d_get_item() {
-  local category=$1
-  declare -a items
-  local items="($($_current_dir/utility/config.py -c $category))"
-  local i=0
-  local item_len=${#items[@]}
-  if [[ $item_len == 0 ]];then
-    echo "get item by category [$_category] falied"
-    exit 1
-  fi
-  while [[ $i -lt $item_len ]]
-  do
-    echo -e "\t$i) ${items[$i]}"
-    let i++
-  done
-  read item_no
-  _host=${items[$item_no]}
+    local category=$1
+    declare -a items
+    local items="($($_current_dir/utility/config.py -c $category))"
+    local i=0
+    local item_len=${#items[@]}
+    if [[ $item_len == 0 ]];then
+        echo "get item by category [$_category] falied"
+        exit 1
+    fi
+    while [[ $i -lt $item_len ]]
+    do
+        echo -e "\t$i) ${items[$i]}"
+        let i++
+    done
+    read item_no
+    _host=${items[$item_no]}
 }
 d_get_host_by_alias() {
-  declare -a hosts
-  local alias=$1
-  local hosts="($($_current_dir/utility/config.py -a $alias))"
-  local i=0
-  local host_len=${#hosts[@]}
-  if [[ $host_len == 0 ]];then
-    echo "get host by alias [$__alias] falied"
-    exit 1
-  fi
-  while [[ $i -lt $host_len ]]
-  do
-    echo -e "\t$i) ${hosts[$i]}"
-    let i++
-  done
-  read host_no
-  _host=${hosts[$host_no]}
+    declare -a hosts
+    local alias=$1
+    local hosts="($($_current_dir/utility/config.py -a $alias))"
+    local i=0
+    local host_len=${#hosts[@]}
+    if [[ $host_len == 0 ]];then
+        echo "get host by alias [$__alias] falied"
+        exit 1
+    fi
+    while [[ $i -lt $host_len ]]
+    do
+        echo -e "\t$i) ${hosts[$i]}"
+        let i++
+    done
+    read host_no
+    _host=${hosts[$host_no]}
 }
 d_get_pass() {
-  if [[ $__pass == "" ]]
-  then
-    echo $($_current_dir/utility/crypt.py -d $_pass)
-  else
-    echo $__pass
-  fi
+    if [[ $__pass == "" ]]
+    then
+        echo $($_current_dir/utility/crypt.py -d $_pass)
+    else
+        echo $__pass
+    fi
 }
 d_get_user() {
-  if [[ $__user == "" ]]
-  then
-    echo $_user
-  else
-    echo $__user
-  fi
+    if [[ $__user == "" ]]
+    then
+        echo $_user
+    else
+        echo $__user
+    fi
+}
+d_get_port() {
+    if [[ $__port == "" ]]
+    then
+        echo $_port
+    else
+        echo $__port
+    fi
 }
 d_get_host() {
-  if [[ $(echo $_host | grep ']') ]]
-  then
-    echo $_host | awk -F ']' '{print $2}'
-  else
-    echo $_host
-  fi
+    if [[ $(echo $_host | grep ']') ]]
+    then
+        echo $_host | awk -F ']' '{print $2}'
+    else
+        echo $_host
+    fi
+}
+
+d_set_profile() {
+    local category=$1
+    local profile="$($_current_dir/utility/profile.py -c $category)"
+    _user="$(echo $profile | awk '{print $1}')"
+    _pass="$(echo $profile | awk '{print $2}')"
+    _port="$(echo $profile | awk '{print $3}')"
 }
 
 d_login() {
-  local host="$(d_get_host)"
-  if [[ $host == "" ]]
-  then
-    echo 'host parameter is required'
-    exit 1
-  fi
-  local user="$(d_get_user)"
-  local pass="$(d_get_pass)"
-  /usr/bin/expect -f $_current_dir/utility/expect.exp $user $pass $host
+    #echo $_category
+    #exit 1
+    local host="$(d_get_host)"
+    if [[ $host == "" ]]
+    then
+        echo 'host parameter is required'
+        exit 1
+    fi
+    local user="$(d_get_user)"
+    local pass="$(d_get_pass)"
+    local port="$(d_get_port)"
+    /usr/bin/expect -f $_current_dir/utility/expect.exp $user $pass $host $port
 }
+
 d_connect() {
   if [[ -n $__host ]];then
     _host=$__host
@@ -162,14 +185,9 @@ d_connect() {
     d_get_category
     d_get_item $_category
   fi
+  d_set_profile $_category
   d_login
 }
 
-. $_current_dir/data/.property
-if [[ $__is_add == 1 ]]
-then
-  #d_add_config
-  echo add _config
-fi
 d_connect
 exit 0
---
+++ utility/profile.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+from ConfigParser import SafeConfigParser
+import os, sys, getopt
+class Profile:
+    def __init__(self, config):
+        self._config = config
+        self._items  = []
+
+    def getProfile(self, category):
+        items = ['user', 'pass', 'port']
+        for item in items:
+            self._items.append(self._config.get(category, item))
+        print(" ".join(self._items))
+
+if __name__ == '__main__':
+    #config = ConfigParser.ConfigParser()
+    config_path = os.path.dirname(__file__)
+    if not config_path:
+        config_path = './'
+    config = SafeConfigParser()
+    
+    config.read(config_path + '/../data/.profile.ini')
+    #-c [category]
+    opts, args = getopt.getopt(sys.argv[1:], "hc:")
+    o = Profile(config)
+
+    category = 'default'
+
+    for op, value in opts:
+        if op == '-c':
+            category = value
+
+    o.getProfile(category)
